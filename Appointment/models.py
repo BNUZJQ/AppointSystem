dcb7e9c26288e29f7bbc5facce28654dc54b0cc7
XuyangBai <653823597@qq.com>
2017-10-02 22:43:29 +0800

    init

---
+++ Appointment/models.py
@@ -0,0 +1,20 @@
+from __future__ import unicode_literals
+
+from Account.models import Account
+from Classroom.models import Classroom
+from django.db import models
+
+TIME_CHOICE = (
+    (0, '0:00-1:00'),
+    (1, '1:00-2:00')
+)
+
+
+# Create your models here.
+class Appointment(models.Model):
+    duration = models.IntegerField(choices=TIME_CHOICE, blank=False)
+    classroom = models.OneToOneField(Classroom, blank=False)
+    custom = models.OneToOneField(Account, blank=False)
+
+    def __unicode__(self):
+        return u'{},{},{}'.format(self.classroom.name, self.duration, self.custom.user.username)
