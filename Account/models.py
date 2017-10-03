525da595528cb0506b9922a96a05a3ed24dc2b62
XuyangBai <653823597@qq.com>
2017-10-03 22:29:27 +0800

    change for account model

--- Account/models.py
+++ Account/models.py
@@ -2,17 +2,24 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class ROLE:
+    Student = 'Student'
+    Teacher = 'Teacher'
+    Admin = 'Admin'
+
+
 ROLE_CHOICE = (
-    (0, 'Student'),
-    (1, 'Teacher'),
-    (2, 'Admin')
+    (ROLE.Student, 'Student'),
+    (ROLE.Teacher, 'Teacher'),
+    (ROLE.Admin, 'Admin')
 )
 
 
 # Create your models here.
 class Account(models.Model):
     user = models.OneToOneField(User)
-    role = models.IntegerField(choices=ROLE_CHOICE, default=0)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICE, default=0)
 
     def __unicode__(self):
         return self.user.username
