c60e232888c5ad95d3dbdd6ccd17e3a48718dc25
XuyangBai <653823597@qq.com>
2017-10-30 20:07:06 +0800

    修复了delete 接口

--- api/views.py
+++ api/views.py
@@ -26,7 +26,9 @@ class ClassroomViewSet(viewsets.GenericViewSet):
         classroom = Classroom.objects.get(name=classroom)
         appointments = classroom.appointment_set.filter(date__gte=today,
                                                         date__lte=endday,
-                                                        classroom__appointment__status__in=(STATUS.waiting, STATUS.opened)).distinct()
+                                                        status=STATUS.waiting).distinct()
+        if 'mine' in request.GET:
+            appointments = appointments.filter(custom__user=user)
         appointments = appointments.values('id',
                                            'reason',
                                            'date',
@@ -37,8 +39,6 @@ class ClassroomViewSet(viewsets.GenericViewSet):
                                            'status',
                                            'custom__user__username',
                                            'custom__telephone')
-        if 'mine' in request.GET:
-            appointments = appointments.filter(custom__user=user)
         size = len(appointments)
         return Response({"success": True,
                          "size": size,
@@ -64,8 +64,18 @@ class ClassroomViewSet(viewsets.GenericViewSet):
             return Response(status=201)
         return Response({"message": appointment.errors}, status=400)
 
-    # delete并非真正删除，而是将status置为canceled
-    def delete(self, request, pk, **kwargs):
+    # # delete并非真正删除，而是将status置为canceled
+    # def delete(self, request, pk, **kwargs):
+    #     classroom = get_object_or_404(Classroom, name=kwargs['classroom'])
+    #     appointment = get_object_or_404(classroom.appointment_set, id=pk)
+    #     appointment.status = STATUS.canceled
+    #     appointment.save()
+    #     return Response({"message": "cancel this appointment"}, status=status.HTTP_204_NO_CONTENT)
+
+    # 这种写法实际上是不符合REST的规范的
+    @csrf_exempt
+    @detail_route(methods=['post'])
+    def delete_appoint(self, request, pk, **kwargs):
         classroom = get_object_or_404(Classroom, name=kwargs['classroom'])
         appointment = get_object_or_404(classroom.appointment_set, id=pk)
         appointment.status = STATUS.canceled
